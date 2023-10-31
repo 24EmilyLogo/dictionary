304b8ba78aede73170876a0ef763854a5eb79dac
StudentAM <studentam@Administrators-MacBook-Pro.local>
2023-10-31 10:22:20 -0700

    Say My Name

--- script.js
+++ script.js
@@ -4,3 +4,50 @@ const doot = {
     sneep: "snoop",
 }
 //the actual stuff
+const words = {
+    yesWord: [
+        "noun",
+        "Used to give an affirmative response.", 
+        "Used as a response to someone addressing one or otherwise trying to attract one's attention."
+    ],
+    panaceaWord: [
+        "noun",
+        "An answer or solution for all problems or difficulties."
+    ],
+    concatenationWord: [
+        "noun",
+        "A series of interconnected or interdependent things or events."
+    ],
+    sawWord:[
+        "verb",
+        "Cut (something) using a saw",
+        "Make rapid to-and-fro motions in cutting something or in playing a stringed instrument"
+    ],
+    foundWord:[
+        "adjective",
+        "Having been discovered by chance or unexpectedly.",
+        "(of a ship) Equipped; Supplied"
+    ],
+    craneWord:[
+        "verb",
+        "Stretch out one's body or neck in order to see something",
+        "Move (a heavy object) with a crane"
+    ],
+    minuteWord:[
+        "noun",
+        "A period of time equal to sixty seconds or a sixtieth of an hour.",
+        "A sixtieth of a degree of angular measurement (symbol: ')"
+    ],
+    grotesqueWord:[
+        "adjective",
+        "Comically or repulsively ugly or distorted."
+    ],
+    labelWord:[
+        "noun",
+        "A small piece of paper, fabric, plastic, or similar material attached to an object and giving information about it"
+    ],
+    debacleWord:[
+        "noun",
+        "A sudden and ignominious failure; a fiasco"
+    ],
+}
