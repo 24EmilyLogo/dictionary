9570bb8e1ffa3acf95c3f5a3bdfd20c65a4a393e
StudentAM <studentam@Administrators-MacBook-Pro.local>
2023-11-02 10:09:14 -0700

    missed some stuff oops

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
 //object with all of the words and definitions
 const words = {
-    yesWord: [
+    yes: [
         "noun",
         "Used to give an affirmative response.", 
         "Used as a response to someone addressing one or otherwise trying to attract one's attention."
     ],
-    panaceaWord: [
+    panacea: [
         "noun",
         "An answer or solution for all problems or difficulties."
     ],
-    concatenationWord: [
+    concatenation: [
         "noun",
         "A series of interconnected or interdependent things or events."
     ],
-    sawWord:[
+    saw:[
         "verb",
         "Cut (something) using a saw",
         "Make rapid to-and-fro motions in cutting something or in playing a stringed instrument"
     ],
-    foundWord:[
+    found:[
         "adjective",
         "Having been discovered by chance or unexpectedly.",
         "(of a ship) Equipped; Supplied"
     ],
-    craneWord:[
+    crane:[
         "verb",
         "Stretch out one's body or neck in order to see something",
         "Move (a heavy object) with a crane"
     ],
-    minuteWord:[
+    minute:[
         "noun",
         "A period of time equal to sixty seconds or a sixtieth of an hour.",
         "A sixtieth of a degree of angular measurement (symbol: ')"
     ],
-    grotesqueWord:[
+    grotesque:[
         "adjective",
         "Comically or repulsively ugly or distorted."
     ],
-    labelWord:[
+    label:[
         "noun",
         "A small piece of paper, fabric, plastic, or similar material attached to an object and giving information about it"
     ],
-    debacleWord:[
+    debacle:[
         "noun",
         "A sudden and ignominious failure; a fiasco"
     ],
