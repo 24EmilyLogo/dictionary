39bf038129151c0e41ce87e24766033a52aec95a
StudentAM <studentam@Administrators-MacBook-Pro.local>
2023-11-07 10:14:33 -0800

    Chant

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
 //object with all of the words and definitions
 const words = {
-    yes: [
+    "yes": [
         "noun",
         "Used to give an affirmative response.", 
         "Used as a response to someone addressing one or otherwise trying to attract one's attention."
     ],
-    panacea: [
+    "panacea": [
         "noun",
         "An answer or solution for all problems or difficulties."
     ],
-    concatenation: [
+    "concatenation": [
         "noun",
         "A series of interconnected or interdependent things or events."
     ],
-    saw:[
+    "saw":[
         "verb",
         "Cut (something) using a saw",
         "Make rapid to-and-fro motions in cutting something or in playing a stringed instrument"
     ],
-    found:[
+    "found":[
         "adjective",
         "Having been discovered by chance or unexpectedly.",
         "(of a ship) Equipped; Supplied"
     ],
-    crane:[
+    "crane":[
         "verb",
         "Stretch out one's body or neck in order to see something",
         "Move (a heavy object) with a crane"
     ],
-    minute:[
+    "minute":[
         "noun",
         "A period of time equal to sixty seconds or a sixtieth of an hour.",
         "A sixtieth of a degree of angular measurement (symbol: ')"
     ],
-    grotesque:[
+    "grotesque":[
         "adjective",
         "Comically or repulsively ugly or distorted."
     ],
-    label:[
+    "label":[
         "noun",
         "A small piece of paper, fabric, plastic, or similar material attached to an object and giving information about it"
     ],
-    debacle:[
+    "debacle":[
         "noun",
         "A sudden and ignominious failure; a fiasco"
     ],
@@ -57,15 +57,22 @@ var wordsInput = document.getElementById("wordInput")
     //1. have code read the input (That's the onclick in the index.html file)
 function findDefinition(){
     //2. change input casing to streamline the code reading it
-//  v TF D'ya want me to do with this?!
-wordsInput.toLowerCase()
-    //3. find if it is a registered word in the object "words"
-    for(i = 0; i < words; i++){
-        if(words[i] === wordsInput){
-            console.log(wordsInput)
-        }
+    if(wordsInput && wordsInput.value){
+        let lcWordsInput = wordsInput.value.toLowerCase();
+        //3. find if it is a registered word in the object "words"
+        // for(i = 0; i < words; i++){
+            if(words.hasOwnProperty(lcWordsInput)){
+                const definition = words[lcWordsInput].join("<br")
+                definitionReplace.innerText = definition;
+            }else{
+                definitionReplace.innerText = "I'm sorry, that word is not registered in the dictionary."
+            }
+    }else{
+        definitionReplace.innerText = "Invalid input"
     }
 
         //a. output word, PoS, & definition if it is in the dictionary
         //b. if not, output "I'm sorry, that word is not registered in the dictionary"
 }
+
+//Ask Is the mama and Not the mama wtf is wrong with my code :/
